3e893e9c2429e76d1ae74960823ddf0ff7adfeeb
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-04-30 13:21:29 -0700

    Create leetcode_948.py
    Really fun brain teaser greedy sorting algorithm here ; the imposition of constraints helped us determine the optimal scoring.

---
+++ leetcode_948.py
@@ -0,0 +1,43 @@
+'''
+948. Bag of Tokens
+URL := https://leetcode.com/problems/bag-of-tokens/description/
+
+Category : Brain Teaser, Sliding Window, Two Pointers, Linear Scanning
+
+Complexity :;
+Let N := #-tokens
+T = O(N)
+S = O(1) ( E & I ) 
+
+
+15 mins to solutioning :-) 
+'''
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        n = len(tokens)
+        tokens.sort()
+        lPtr = 0
+        rPtr = n - 1
+        # Check our while statement later.
+        # wait what if we get a case where we attempted to go up in score - but it didn't work too?
+        # idea : keep track of a running max global score as well
+        curScore = 0
+        maxRunScore = 0
+        while(lPtr <= rPtr):
+            if(power >= tokens[lPtr] and lPtr <= rPtr):
+                power -= tokens[lPtr]
+                curScore += 1
+                lPtr += 1
+            elif( power < tokens[lPtr]):
+                if(curScore >= 1):
+                    if(rPtr > lPtr):
+                        power += tokens[rPtr]
+                        rPtr -= 1
+                        curScore -= 1
+                    elif(lPtr <= rPtr):
+                        break
+                else:
+                    # hit terminal state : can not increase power nor execute left players <--- problem constraint
+                    break
+            maxRunScore = max(maxRunScore, curScore)
+        return maxRunScore
