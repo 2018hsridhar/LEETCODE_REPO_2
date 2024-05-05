71abcf02062bfcab967c32dfb4fe4ebd7642c21d
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-05-05 13:09:38 -0700

    Update leetcode_3137.py
    I condensed the code ; it's more readable now.
    Also to say it : `int(possiblyFloat)` is more indicative of an explicit conversion, versus `floor(float)`

--- leetcode_3137.py
+++ leetcode_3137.py
@@ -3,25 +3,21 @@
 URL := https://leetcode.com/problems/minimum-number-of-operations-to-make-word-k-periodic/description/
 
 https://stackoverflow.com/questions/3294889/iterating-over-dictionaries-using-for-loops
-
+Woah there's a lot of values to unpack here; we need to unpack dictionaries and abstractions in python3
 '''
 class Solution:
     def minimumOperationsToMakeKPeriodic(self, word: str, k: int) -> int:
         n = len(word)
         minOps = n
         periodicHits = dict()
-        for leftPtr in range(len(word)):
+        # int(possiblyFloat) more indicative of an explicit conversion ( versus floor(float))
+        numberPrefixesToMake = int(n / k)
+        for leftPtr in range(0,n-k+1,1):
             # Double equals numeric comparison in Py3
             if(leftPtr % k == 0):
-                rightPtr = leftPtr + k
-                if(rightPtr <= n):
-                    curPrefix = word[leftPtr:rightPtr]
-                    if curPrefix not in periodicHits:
-                        periodicHits[curPrefix] = 0
-                    periodicHits[curPrefix] += 1
-        numberPrefixesToMake = floor(n / k)
-        # woah there's a lot of values to unpack here; we need to unpack dictionaries and abstractions in python3
-        for prefix, prefixIdxHits in periodicHits.items():
-            minOps = min(minOps, numberPrefixesToMake - prefixIdxHits )
+                curPrefix = word[leftPtr:leftPtr + k]
+                if curPrefix not in periodicHits:
+                    periodicHits[curPrefix] = 0
+                periodicHits[curPrefix] += 1
+                minOps = min(minOps, numberPrefixesToMake - periodicHits[curPrefix] )
         return minOps
-        
