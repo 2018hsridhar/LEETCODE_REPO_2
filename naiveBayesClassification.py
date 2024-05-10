4ed86a438e8de807e54c7504c1472320fb1305a0
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-05-09 17:27:11 -0700

    Update naiveBayesClassification.py

--- naiveBayesClassification.py
+++ naiveBayesClassification.py
@@ -1,12 +1,1 @@
-'''
-Working with Python Jupyter Notebooks
-Goal : To Explore Supervised Learning Methods -> in this case, Naive Bayes Classification
-We predicate many SL methods off of the NB method
 
-Setting up Pythonic Naive Bayes Classification.
-Let's explore how to evolve and iterate on a classificier from two class ( binomial ) to multi class.
-We'll also explore the methods of TF ( Total Frequency ), IDF ( Inverse Document Frequency ), and TF-IDF ( which combines the two as a product ).
-Lastly, we'll also explore vectorization of counts based on a vocabulary of words, and representing non-mathematical inputs more mathematically.
-
-
-'''
