10348ebc9f997bc3ad72dce6faf3674a517855e2
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-06-18 08:22:01 -0700

    Update leetcode_3128.py

--- leetcode_3128.py
+++ leetcode_3128.py
@@ -1,68 +1,51 @@
 '''
-URL := https://leetcode.com/problems/right-triangles/description/
 3128. Right Triangles
+URL := https://leetcode.com/problems/right-triangles/description/
 
+Seems prefix sum based TBH ( elements need not have adjacency to one another )
 Complexity
-Let M, N := dims(grid)
-Time := O(MN)
-S := O(1) ( I & E ) 
+Let M, N := dim (grid )
+T = O(MN)
+S = O(MN) ( E ) O(1) ( I ) 
 
-Commit Message :
-    # Why is PY3 ruled by definitions and lambda styles for functions?
-    # Typing out 'and' and 'or' seen as safer lang choices over '&' and '|' statements.
-    # A semi-colon ':' governed language
-    # self : referential to the name of own class ( in this case, Solution )
-
-Oh -> it's actuall a count of 1's to the right and to the left : the problem simplifies
-
-15 minutes -> go do this problem later. 
 '''
 class Solution:
     def numberOfRightTriangles(self, grid: List[List[int]]) -> int:
-        nort = 0
-        # 0-init list comp style
-        oneCountBelow = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-        oneCountAbove = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-        oneCountRight = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-        oneCountLeft = [[0 for i in range(len(grid))] for j in range(len(grid[0]))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                nort += self.getNorthEastStatus(grid,i,j)
-                nort += self.getSouthEastStatus(grid,i,j)
-                nort += self.getNorthWestStatus(grid,i,j)
-                nort += self.getSouthWestStatus(grid,i,j)
-        return nort
-    
-    def getNorthEastStatus(self, grid: List[List[int]], x:int, y:int) -> int:
-        status = 0
-        if(self.isInBounds(grid,x,y) and self.isInBounds(grid,x+1,y) and self.isInBounds(grid,x+1,y+1)):
-            if(grid[x][y] == 1 and grid[x+1][y] == 1 and grid[x+1][y+1] == 1):
-                status = 1
-        return status
-
-    def getSouthEastStatus(self, grid: List[List[int]], x:int, y:int) -> int:
-        status = 0
-        if(self.isInBounds(grid,x,y) and self.isInBounds(grid,x-1,y) and self.isInBounds(grid,x-1,y+1)):
-            if(grid[x][y] == 1 and grid[x-1][y] == 1 and grid[x-1][y+1] == 1):
-                status = 1
-        return status
-
-    def getNorthWestStatus(self, grid: List[List[int]], x:int, y:int) -> int:
-        status = 0
-        if(self.isInBounds(grid,x,y) and self.isInBounds(grid,x+1,y) and self.isInBounds(grid,x+1,y-1)):
-            if(grid[x][y] == 1 and grid[x+1][y] == 1 and grid[x+1][y-1] == 1):
-                status = 1
-        return status
-
-    def getSouthWestStatus(self, grid: List[List[int]], x:int, y:int) -> int:
-        status = 0
-        # written as for loop -> fixed range -> avoid branch logic -> more optimized under hood
-        if(self.isInBounds(grid,x,y) and self.isInBounds(grid,x-1,y) and self.isInBounds(grid,x-1,y-1)):
-            if(grid[x][y] == 1 and grid[x-1][y] == 1 and grid[x-1][y-1] == 1):
-                status = 1
-        return status
-
-    def isInBounds(self, grid: List[List[int]], x:int, y:int) -> bool:
+        numRightTri = 0
         m = len(grid)
         n = len(grid[0])
-        return (0 <= x and x < m) and (0 <= y and y < n)
+        numOnesNorth = [[0 for j in range(n)] for i in range(m)]
+        numOnesSouth = [[0 for j in range(n)] for i in range(m)]
+        numOnesEast = [[0 for j in range(n)] for i in range(m)]
+        numOnesWest = [[0 for j in range(n)] for i in range(m)]
+        # [1] populate ones north
+        # [2] populate ones south
+        for c in range(n):
+            oneNorth = 0
+            oneSouth = 0
+            for r in range(m):
+                numOnesNorth[r][c] = oneNorth
+                oneNorth += (int)(grid[r][c])
+            for r in range(m-1,-1,-1):
+                numOnesSouth[r][c] = oneSouth
+                oneSouth += (int)(grid[r][c])
+        # [3] populate ones west
+        # [4] populate ones east
+        for r in range(m):
+            oneWest = 0
+            for c in range(n):
+                numOnesWest[r][c] = oneWest
+                oneWest += (int)(grid[r][c])
+            oneEast = 0
+            for c in range(n-1,-1,-1):
+                numOnesEast[r][c] = oneEast
+                oneEast += (int)(grid[r][c])
+        for r in range(m):
+            for c in range(n):
+                if(grid[r][c] == 1):
+                    numNorthEast = numOnesNorth[r][c] * numOnesEast[r][c]
+                    numSouthEast = numOnesSouth[r][c] * numOnesEast[r][c]
+                    numNorthWest = numOnesNorth[r][c] * numOnesWest[r][c]
+                    numSouthWest = numOnesSouth[r][c] * numOnesWest[r][c]
+                    numRightTri += sum([numNorthEast, numSouthEast, numNorthWest, numSouthWest])
+        return numRightTri
