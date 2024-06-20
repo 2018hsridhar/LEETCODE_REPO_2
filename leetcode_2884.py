8b354347b9154d24dd6de77948e306f55aa0b897
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-06-20 11:07:21 -0700

    Create leetcode_2884.py
    Gracefulness in columnar multiplication of tabulated data.

---
+++ leetcode_2884.py
@@ -0,0 +1,6 @@
+# 2884. Modify Columns
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary'].apply(lambda x : x*2)
+    return employees
